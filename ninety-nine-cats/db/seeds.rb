728958980362891f1e8de1b3e19f754bfd786249
Jason <jasonzhang501@gmail.com>
2023-09-29 16:47:55 -0400

    Up To ShowPage

--- ninety-nine-cats/db/seeds.rb
+++ ninety-nine-cats/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Cat.destroy_all
+
+c1 = Cat.create!(birth_date: '2020/01/01', name: 'Jeff', color: 'purple', sex:'M')
+c2 = Cat.create!(birth_date: '2021/02/02', name: 'Dustin', color: 'white', sex:'F')
+c3 = Cat.create!(birth_date: '2022/03/03', name: 'Eric', color: 'black', sex:'F')
